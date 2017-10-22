af83a10277e7cf780c11127d8423d22817f6b058
ryo64 <32954069+ryo64@users.noreply.github.com>
2017-10-22 11:17:17 +0900

    UI調整・Imgフォルダにタイトルロゴ追加

---
+++ GameJam2017/Assets/ryo_UI_0/Script/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	float countTime = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countTime += Time.deltaTime;
+		GetComponent<Text>().text = countTime.ToString("F0");
+		
+	}
+}
